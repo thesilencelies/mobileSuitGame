28b9678870ea18904955b2d773ce92f24e9b3d17
Stephen Lilico <gonetothewind@gmail.com>
2017-11-08 22:42:00 +0000

    some tidying up

--- generateCards.py
+++ generateCards.py
@@ -4,7 +4,7 @@ import csv
 
 datafile = 'data.csv'
 
-outputfolder='gundamCards/objects/card_'
+outputfolderr='objects/card_'
 with open(datafile) as csvfile:
      reader = csv.DictReader(csvfile)
      i = 0
@@ -15,20 +15,22 @@ with open(datafile) as csvfile:
             #background image should be loaded here?
             filetext = "\\begin{tikzpicture}[]\n "
             #format the card
-             #default symbols
-             #lightning motif
-             filetext = filetext + "\\draw()"
-             filetext = filetext + "\\node[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30blue!70, line =white!40blue!60, draw] (1, 2){" + row['inititive'] +"}" 
+            #default symbols
+            #lightning motif
+            filetext = filetext + "\\draw()"
+            filetext = filetext + "\\node[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30blue!70, line =white!40blue!60, draw] (1, 2){" + row['inititive'] +"};" 
             
             if row["isBasic"] == "y":
-                 filetext = filtext + "\\node "
-             #attacks
+                 filetext = filtext + "\\node[text colour = green]{\\Large{B}};"
+            #attacks
+            
+
+            #ranges
              
-             #ranges
+            #blocks
              
-             #blocks
-             
-             #cardtext - needs tex commands to make it multilines
-             filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, draw, text width = 5cm] (1, 20){" + row['text'] +"}" 
+            #cardtext - needs tex commands to make it multilines
+            filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, draw, text width = 5cm] (1, 20){" + row['text'] +"};" 
+            filetext = filetext + "\\end{tikzpicture}"
             ofile.write(filetext)
-             print(row['first_name'], row['last_name'])
+
