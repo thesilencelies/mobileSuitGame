1a28534cbcf6ea07ecd6cbfa40ff5521ac5a32fb
Stephen Lilico <gonetothewind@gmail.com>
2017-11-09 00:16:28 +0000

    initial working pass at generating something, errors in the tikz code

--- generateCards.py
+++ generateCards.py
@@ -4,24 +4,24 @@ import csv
 
 datafile = 'data.csv'
 
-outputfolderr='objects/card_'
+outputfolder='objects/card_'
 with open(datafile) as csvfile:
      reader = csv.DictReader(csvfile)
      i = 0
      for row in reader:
          #CONSTRUCT A NEW CARD
-         with open(outputfolder + str(i) + '.tex') as ofile:
+         with open(outputfolder + str(i) + '.tex', 'w') as ofile:
             i = i + 1
             #background image should be loaded here?
             filetext = "\\begin{tikzpicture}[]\n "
             #format the card
             #default symbols
             #lightning motif
-            filetext = filetext + "\\draw()"
-            filetext = filetext + "\\node[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30blue!70, line =white!40blue!60, draw] (1, 2){" + row['inititive'] +"};" 
+            filetext = filetext + "\\draw[line = blue](1.1,1.9) -- (1.05,1.95)\n"
+            filetext = filetext + "\\node[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30blue!70, line = white!40blue!60, draw] (1, 2){" + row['Initiative'] +"};\n" 
             
-            if row["isBasic"] == "y":
-                 filetext = filtext + "\\node[text colour = green]{\\Large{B}};"
+            if row["Basicness"] == "y":
+                 filetext = filetext + "\\node[text colour = green]{\\Large{B}};\n"
             #attacks
             
 
@@ -30,7 +30,7 @@ with open(datafile) as csvfile:
             #blocks
              
             #cardtext - needs tex commands to make it multilines
-            filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, draw, text width = 5cm] (1, 20){" + row['text'] +"};" 
-            filetext = filetext + "\\end{tikzpicture}"
+            filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, draw, text width = 5cm] (1, 20){" + row['Text'] +"};\n" 
+            filetext = filetext + "\\end{tikzpicture}\n"
             ofile.write(filetext)
 
