ef475ecd74fe734b3f2a8e919dea76cfa42f0c14
thesilencelies <gonetothewind@gmail.com>
2017-11-21 23:37:42 +0000

    Update generateCards.py

--- generateCards.py
+++ generateCards.py
@@ -4,12 +4,12 @@ import csv
 
 datafile = 'data.csv'
 
-mAtkImg = ''
-rAtkImg = ''
-blkImg = ''
-rangeImg = ''
-initImg = ''
-mvImg = ''
+mAtkImg = 'attackImg.png'
+rAtkImg = 'rattackImg.png'
+blkImg = 'blockImg.png'
+rangeImg = 'rangeImg.png'
+initImg = 'initImg.png'
+mvImg = 'mvimg.png'
 
 outputfolder='objects/card_'
 with open(datafile) as csvfile:
@@ -18,62 +18,70 @@ with open(datafile) as csvfile:
      for row in reader:
          #CONSTRUCT A NEW CARD
          with open(outputfolder + str(i) + '.tex', 'w') as ofile:
+            print("starting card " + str(i))
             i = i + 1
             #background image should be loaded here?
             filetext = "\\begin{tikzpicture}[scale=0.5]\n "
+            filetext = filetext + '\\node(0,0){\\includegraphics[width=8cm]{' + row["BackgroundImg"] + '}};\n'
             #format the card
             #default symbols
             #lightning motif
-            filetext = filetext + "\\draw[blue](1.1,1.9) -- (1.05,1.95)\n"
-            #filetext = filetext + '\\node(1,2){\\includegraphics("initiativeImg.png")}\n'
-            filetext = filetext + "\\node[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30blue!70, line = white!40blue!60, draw] (1, 2){}; \\node(1,2){" + row['Initiative'] +"};\n" 
-            filetext = filetext + "\\node[rectangle, minimum width = 1cm, fill opacity = 0.3, fill = white!30yellow!70, line = white!40yellow!60, draw] (1, 2){}; \\node(1,2){" + row['Movement'] +"};\n" 
+            #filetext = filetext + "\\draw[blue](1.1,1.9) -- (1.05,1.95)\n"
+            filetext = filetext + '\\node(1,2){\\includegraphics{' + initImg + '}};\n'
+            filetext = filetext + "\\node(1,2)[circle, minimum width = 1cm, fill opacity = 0.3, fill = white!30!blue!70, color = white!40!blue!60, draw] {}; \\node(1,2){" + row['Initiative'] +"};\n" 
+            #filetext = filetext + "\\node[rectangle, minimum width = 1cm, fill opacity = 0.3, fill = white!30!yellow!70, color = white!40!yellow!60, draw] (1, 2){};"
+            filetext = filetext + '\\node(1,3){\\includegraphics{' + mvImg + '}};\n'
+            filetext = filetext + " \\node(1,3){" + row['Movement'] +"};\n" 
             if row["Basicness"] == "y":
-                 filetext = filetext + "\\node[text colour = green]{\\Large{B}};\n"
+                 filetext = filetext + "\\node (1,4)[text = green]{\\Large{B}};\n"
+                 
+            print("starting attacks")
             #attacks
             if int(row["HighAttack"]) or int(row["HighBlock"]):
-                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, round corners, fill=white, opacity=0.3](4, 2){};\n"
+                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, rounded corners=0.3cm, fill=white, opacity=0.3](4, 2){};\n"
+            aimg = rAtkImg if int(row["HighRange"]) > 0 else mAtkImg
             for d in range(0,int(row["HighAttack"])):
-                filetext = filetext + "\\node(" + str(-i + 7) + ', 1){\\includegraphics{"attackimg.png"}};\n'
+                filetext = filetext + "\\node(" + str(-i + 7) + ', 1){\\includegraphics{' + aimg + '}};\n'
             
             #blocks
             for d in range(0,int(row["HighBlock"])):
-                filetext = filetext + "\\node(" + str(i - 7) + ', 2){\\includegraphics{"blockImg.png"}};\n'
+                filetext = filetext + "\\node(" + str(i - 7) + ', 2){\\includegraphics{' + blkImg + '}};\n'
             #ranges
             if int(row["HighRange"]) > 0:
-                 filetext = filetext + '\\node( 7 , 2){\\includegraphics{"rangeimg.png"}};\n'
+                 filetext = filetext + '\\node( 7 , 2){\\includegraphics{' + rangeImg + '}};\n'
                  filetext = filetext + '\\node(7, 3){' + row["HighRange"] + '};\n'
-             
+            print("mid")
               #attacks
             if int(row["MidAttack"]) or int(row["MidBlock"]):
-                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, round corners, fill=white, opacity=0.3](4, 5){};\n"
+                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, rounded corners= 0.3cm, fill=white, opacity=0.3](4, 5){};\n"
+            aimg = rAtkImg if int(row["MidRange"]) > 0 else mAtkImg
             for d in range(0,int(row["MidAttack"])):
-                filetext = filetext + "\\node(" + str(-i + 7) + ', 5){\\includegraphics{"attackimg.png"}};\n'
+                filetext = filetext + "\\node(" + str(-i + 7) + ', 5){\\includegraphics{'+ aimg + '}};\n'
             
             #blocks
             for d in range(0,int(row["MidBlock"])):
-                filetext = filetext + "\\node(" + str(i - 7) + ', 6){\\includegraphics{"blockImg.png"}};\n'
+                filetext = filetext + "\\node(" + str(i - 7) + ', 6){\\includegraphics{' + blkImg + '}};\n'
             #ranges
             if int(row["MidRange"]) > 0:
-                 filetext = filetext + '\\node( 7 , 5){\\includegraphics{"rangeimg.png"}};\n'
+                 filetext = filetext + '\\node( 7 , 5){\\includegraphics{' + rangeImg + '}};\n'
                  filetext = filetext + '\\node(7, 5){' + row["MidRange"] + '};\n'
-             
+            print("low")
               #attacks
             if int(row["LowAttack"]) or int(row["LowBlock"]):
-                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, round corners, fill=white, opacity=0.3](4, 8){};\n"
+                filetext = filetext + "\\node[rectangle, minimum height = 1cm, minimum width =2cm, rounded corners = 0.3cm, fill=white, opacity=0.3](4, 8){};\n"
+            aimg = rAtkImg if int(row["LowRange"]) > 0 else mAtkImg
             for d in range(0,int(row["LowAttack"])):
-                filetext = filetext + "\\node(" + str(-i + 7) + ', 7){\\includegraphics{"attackimg.png"}};\n'
+                filetext = filetext + "\\node(" + str(-i + 7) + ', 7){\\includegraphics{' + aimg + '}};\n'
             
             #blocks
             for d in range(0,int(row["LowBlock"])):
-                filetext = filetext + "\\node(" + str(i - 7) + ', 8){\\includegraphics{"blockImg.png"}};\n'
+                filetext = filetext + "\\node(" + str(i - 7) + ', 8){\\includegraphics{' + blkImg + '}};\n'
             #ranges
             if int(row["LowRange"]) > 0:
-                 filetext = filetext + '\\node( 7 , 7){\\includegraphics{"rangeimg.png"}};\n'
+                 filetext = filetext + '\\node( 7 , 7){\\includegraphics{' + rangeImg + '}};\n'
                  filetext = filetext + '\\node(7, 8){' + row["LowRange"] + '};\n'
-             
+            print("text")
             #cardtext
-            filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, draw, text width = 5cm] (1, 20){" + row['Text'] +"};\n" 
+            filetext = filetext + "\\node[rectangle, fill opacity = 0.3, fill = white, rounded corners = 0.3cm, draw, text width = 5cm] (1, 20){" + row['Text'] +"};\n" 
             filetext = filetext + "\\end{tikzpicture}\n"
             ofile.write(filetext)
-
