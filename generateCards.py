9fad98e26ae170d856744b6f17bee6731a2d62c6
Stephen Lilico <lilico@acsl.com>
2026-01-23 19:24:04 +0900

    [fix](rules): rules cleanup

--- generateCards.py
+++ generateCards.py
@@ -7,7 +7,8 @@ general_action_file = 'Gundam Cards - Default actions.csv'
 pilot_actions_file = 'Gundam Cards - Pilot actions.csv'
 booster_actions_file = 'Gundam Cards - Booster actions.csv'
 frames_file = 'Gundam Cards - Frames.csv'
-outputfolder='build/card_'
+cardoutputfolder='build/card_'
+frameoutputfolde='build/frame_'
 
 #icon names
 mAtkImg = 'attackImg.png'
@@ -49,15 +50,15 @@ def attack_box(atk, rng, block, pos):
                    blkImg + '}};\n'
     # ranges
     if rng > 0:
-        out_text = out_text + '\\node at ( 6 , ' + str(pos - 0.55) + '){\\includegraphics[' + iconwidth + ']{' + \
+        out_text = out_text + '\\node at ( 5.9, ' + str(pos - 0.55) + '){\\includegraphics[' + iconwidth + ']{' + \
                    icons_folder + rangeImg + '}};\n'
-        out_text = out_text + '\\node at (6, ' + str(pos + 0.1) + '){\\Large{' + str(rng) + '}};\n'
+        out_text = out_text + '\\node at (5.9, ' + str(pos - 0.1) + '){\\Large{' + str(rng) + '}};\n'
 
     return out_text
 
 
 def make_card_from_row(row, i):
-    with open(outputfolder + str(i) + '.tex', 'w') as ofile:
+    with open(cardoutputfolder + str(i) + '.tex', 'w') as ofile:
         # print("starting card " + str(i))
         # print(row)
 
@@ -98,7 +99,7 @@ def make_card_from_row(row, i):
         return card_text + "~"
     
 def make_pilot_from_row(row, i):
-    with open(outputfolder + str(i) + '.tex', 'w') as ofile:
+    with open(cardoutputfolder + str(i) + '.tex', 'w') as ofile:
         # print("starting card " + str(i))
         # print(row)
 
@@ -131,50 +132,52 @@ def make_pilot_from_row(row, i):
                 +  row['Slot type'] + " : " + row['Slot name'] + "};\n"
 
         card_text = card_text + "\\end{tikzpicture}\n"
-        ofile.write(header_text)
+        # ofile.write(header_text)
         ofile.write(card_text)
-        ofile.write("\\end{document}\n")
+        # ofile.write("\\end{document}\n")
         return card_text + "~"
     
 
-def create_frame_sheet(frame):
-    """creates the frames datasheet procedurally from the given data"""
-    #load the initial image
-    frame_text = "\\begin{tikzpicture}[scale=0.86, backbox/.style= {rectangle, minimum height = 2.2cm," \
-               + " minimum width =2.2cm, rounded corners = 0.3cm, fill=white, opacity=0.75}]\n "
-    frame_text = frame_text + "\\node [rectangle, minimum width = 6.2cm, minimum height = 8.5cm, fill=black!70!white!30] at (4,5){};\n"
-    frame_text = frame_text + '\\node at (4,5){\\includegraphics[width=6cm, max height = 8.5cm, keepaspectratio]{' + images_folder + frame["BackgroundImg"] + '}};\n'
-    # name
-    frame_text = frame_text + "\\node [rectangle, minimum width=4.3cm, minimum height = 1cm,rounded corners = 0.1cm, fill=white, opacity=0.75] at (3.3, 9){\\large{" + frame["Name"] + "}};\n"
-    
-    # movement
-    frame_text = frame_text + '\\node at (7,9){\\includegraphics[' + iconwidth + ']{' + icons_folder + mvImg + '}};\n'
-    frame_text = frame_text + " \\node at (7,9){\\Large{\\textbf{" + frame['Movement'] +"}}};\n"
-    
-    # armor
-    frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 7.5){"+ frame["Top armour"]+"};\n"
-    frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 5.5){"+ frame["Side armour"]+"};\n"
-    frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 3.5){"+ frame["Low armour"]+"};\n"
+def create_frame_sheet(frame, i):
+    with open(frameoutputfolde + str(i) + '.tex', 'w') as ofile:
+        """creates the frames datasheet procedurally from the given data"""
+        #load the initial image
+        frame_text = "\\begin{tikzpicture}[scale=0.86, backbox/.style= {rectangle, minimum height = 2.2cm," \
+                + " minimum width =2.2cm, rounded corners = 0.3cm, fill=white, opacity=0.75}]\n "
+        frame_text = frame_text + "\\node [rectangle, minimum width = 6.2cm, minimum height = 8.5cm, fill=black!70!white!30] at (4,5){};\n"
+        frame_text = frame_text + '\\node at (4,5){\\includegraphics[width=6cm, max height = 8.5cm, keepaspectratio]{' + images_folder + frame["BackgroundImg"] + '}};\n'
+        # name
+        frame_text = frame_text + "\\node [rectangle, minimum width=4.3cm, minimum height = 1cm,rounded corners = 0.1cm, fill=white, opacity=0.75] at (3.3, 9){\\large{" + frame["Name"] + "}};\n"
+        
+        # movement
+        frame_text = frame_text + '\\node at (7,9){\\includegraphics[' + iconwidth + ']{' + icons_folder + mvImg + '}};\n'
+        frame_text = frame_text + " \\node at (7,9){\\Large{\\textbf{" + frame['Movement'] +"}}};\n"
+        
+        # armor
+        frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 7.5){"+ frame["Top armour"]+"};\n"
+        frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 5.5){"+ frame["Side armour"]+"};\n"
+        frame_text = frame_text + "\\node [rectangle, minimum width=2cm, minimum height = 1cm, fill = red, opacity = 0.75] at (6.5, 3.5){"+ frame["Low armour"]+"};\n"
 
-    # ability
-    frame_text = frame_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum height =1.5cm, rounded corners = 0.3cm, " \
-                + "text width = 3.5cm]  at (3, 3.5){\\small{" + frame['Abilities'] +"}};\n"
+        # ability
+        frame_text = frame_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum height =1.5cm, rounded corners = 0.3cm, " \
+                    + "text width = 3.5cm]  at (3, 3.5){\\small{" + frame['Abilities'] +"}};\n"
 
-    # weapons
-    frame_text = frame_text + "\\node [rectangle, rounded corners = 0.3cm, minimum width=5cm, minimum height = 1cm, fill = white,  opacity = 0.75] at (4, 1.2){" + \
-            '\\includegraphics[' + iconwidth + ']{' + icons_folder + weaponImg + '} \\large{ : ' + str(frame["Weapon Slots"]) +  \
-            '} ~\\includegraphics[' + iconwidth + ']{' + icons_folder + boosterImg + '}\\large{  : ' + str(frame["Boosters"]) + "}};\n"
+        # weapons
+        frame_text = frame_text + "\\node [rectangle, rounded corners = 0.3cm, minimum width=5cm, minimum height = 1cm, fill = white,  opacity = 0.75] at (4, 1.2){" + \
+                '\\includegraphics[' + iconwidth + ']{' + icons_folder + weaponImg + '} \\large{ : ' + str(frame["Weapon Slots"]) +  \
+                '} ~\\includegraphics[' + iconwidth + ']{' + icons_folder + boosterImg + '}\\large{  : ' + str(frame["Boosters"]) + "}};\n"
 
 
-    #finish the tikzpicture
-    frame_text = frame_text + "\\end{tikzpicture}\n"
+        #finish the tikzpicture
+        frame_text = frame_text + "\\end{tikzpicture}\n"
 
-    return frame_text + "~"
+        ofile.write(frame_text)
+        return frame_text + "~"
 
 
 #the actual run
 if __name__ == "__main__":
-    with open(outputfolder + "all.tex", "w") as allfile:
+    with open(cardoutputfolder + "all.tex", "w") as allfile:
         allfile.write(header_text)
         i = 0
         with open(weapon_actions_file, "r") as spcsvfile:
@@ -206,13 +209,13 @@ if __name__ == "__main__":
                     for img in frameBackgrounds:
                         row["BackgroundImg"] = img
                         allfile.write(make_card_from_row(row, i))
-
+        j = 0
         with open(frames_file, "r") as fcsvfile:
             reader = csv.DictReader(fcsvfile)
             allfile.write("\\newpage \n")
             for row in reader:
-                i = i + 1
+                j = j + 1
                 if int(row["Changed"]) > 0:
-                    allfile.write(create_frame_sheet(row))
+                    allfile.write(create_frame_sheet(row, j))
 
         allfile.write("\\end{document}\n")
