a0c0ae9398026215b120be002977addab5df3f13
Stephen Lilico <lilico@acsl.com>
2026-03-04 10:22:42 +0900

    start refactoring away from gundam

--- generateCards.py
+++ generateCards.py
@@ -1,12 +1,13 @@
 #Reads the given CSV of card designs and turns them into TEX files that could be imported as needed
 
 import csv
+import enum
 
-weapon_actions_file = 'Gundam Cards - Weapon actions.csv'
-general_action_file = 'Gundam Cards - Default actions.csv'
-pilot_actions_file = 'Gundam Cards - Pilot actions.csv'
-booster_actions_file = 'Gundam Cards - Booster actions.csv'
-frames_file = 'Gundam Cards - Frames.csv'
+weapon_actions_file = 'Weapon actions.csv'
+general_action_file = 'Basic actions.csv'
+pilot_actions_file = 'Pilot actions.csv'
+booster_actions_file = 'Booster actions.csv'
+frames_file = 'Frames.csv'
 cardoutputfolder='build/card_'
 frameoutputfolde='build/frame_'
 
@@ -30,7 +31,17 @@ frameBackgrounds = ["Hekija_1.jpg","Reginglaze_1.jpg", "Barbatos_1.jpg",
 iconwidth ="width=0.9cm"
 
 header_text = "\\documentclass[a4paper, landscape]{article}\n \\usepackage[left =2cm, right = 2cm, " \
-            + "top = 1.4cm, bottom =1.4cm]{geometry} \n \\usepackage{tikz} \n \\usepackage[export]{adjustbox} \n \\usetikzlibrary{positioning} \n\\begin{document}\n\\noindent\n"
+            + "top = 1.4cm, bottom =1.4cm]{geometry} \n \\usepackage{tikz} \n \\usepackage[export]{adjustbox}" \
+            + "\n \\usetikzlibrary{positioning} \n\\begin{document}\n\\noindent\n"
+
+class CardTypeEnum(enum.Enum):
+    BASIC = 0
+    WEAPON = 1
+    PILOT = 2
+    BOOSTER = 3
+
+def getTypeName(t: CardTypeEnum):
+    return str(t).split(".")[-1].lower().capitalize()
 
 def attack_box(atk, rng, block, pos):
     out_text = ""
@@ -57,16 +68,24 @@ def attack_box(atk, rng, block, pos):
     return out_text
 
 
-def make_card_from_row(row, i):
+def make_card_from_row(row, i, card_type):
     with open(cardoutputfolder + str(i) + '.tex', 'w') as ofile:
-        # print("starting card " + str(i))
-        # print(row)
-
+        # art and card edge
         card_text = "\\begin{tikzpicture}[scale=0.86, backbox/.style= {rectangle, minimum height = 2.0cm," \
                    + " minimum width =2.0cm, rounded corners = 0.3cm, fill=white, opacity=0.75}]\n "
         card_text = card_text + "\\node [rectangle, minimum width = 6.2cm, minimum height = 8.5cm, fill=black] at (4,5){};\n"
         card_text = card_text + '\\node at (4,5){\\includegraphics[width=6cm, max height = 8.3cm, keepaspectratio]{' + images_folder + row["BackgroundImg"] + '}};\n'
-        # format the card
+        # frame style
+        if card_type is CardTypeEnum.BOOSTER:
+            # TODO - create booster frame
+            pass
+        if card_type is CardTypeEnum.PILOT:
+            # TODO - create pilot frame
+            pass
+        if card_type is CardTypeEnum.WEAPON:
+            # TODO - create weapon frame
+            pass
+        
         # name
         card_text = card_text + "\\node [rectangle, minimum width=4cm, minimum height = 0.6cm,rounded corners = 0.1cm, fill=white, opacity=0.75] at (4, 9.2){\\large{" + row["Name"] + "}};\n"
         # default symbols
@@ -78,58 +97,27 @@ def make_card_from_row(row, i):
         if int(row["OneUse"]) > 0:
              card_text = card_text + "\\node at (7,9.2)[circle, fill = red]{\\large{\\textbf{O}}};\n"
 
-        card_text = card_text + attack_box(int(row["HighAttack"]), int(row["HighRange"]), int(row["HighBlock"]), 7.5)
-        card_text = card_text + attack_box(int(row["MidAttack"]), int(row["MidRange"]), int(row["MidBlock"]), 5.0)
-        card_text = card_text + attack_box(int(row["LowAttack"]), int(row["LowRange"]), int(row["LowBlock"]), 2.5)
+        if card_type is CardTypeEnum.PILOT:
+            card_text = card_text + attack_box(0, 0, 1, 7.5)
+        else:
+            card_text = card_text + attack_box(int(row["HighAttack"]), int(row["HighRange"]), int(row["HighBlock"]), 7.5)
+            card_text = card_text + attack_box(int(row["MidAttack"]), int(row["MidRange"]), int(row["MidBlock"]), 5.0)
+            card_text = card_text + attack_box(int(row["LowAttack"]), int(row["LowRange"]), int(row["LowBlock"]), 2.5)
 
         # textbox
-        if row["Text"]:
+        if card_type is CardTypeEnum.PILOT:
             card_text = card_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum height =1.5cm, rounded corners = 0.3cm, " \
+                    + "text width = 5.4cm]  at (4, 3.5){\\small{" + row['Text'] +"}g(c};\n"
+        else:
+            if row["Text"]:
+                card_text = card_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum height =1.5cm, rounded corners = 0.3cm, " \
                     + "text width = 3.5cm]  at (2.75, 3.5){\\small{" + row['Text'] +"}};\n"
+        
         #set info
         card_text = card_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum width=6cm, minimum height =0.8cm, " \
                 + "rounded corners = 0.3cm, text width = 5.8cm]  at (4, 0.7){" \
-                +  row['Slot type'] + " :  " + row['Slot name'] + "};\n"
+                + row["Faction"] + " " + getTypeName(card_type) +  " \hfill " + row['Group'] + "};\n"
 
-
-        card_text = card_text + "\\end{tikzpicture}\n"
-        # ofile.write(header_text)
-        ofile.write(card_text)
-        # ofile.write("\\end{document}\n")
-        return card_text + "~"
-    
-def make_pilot_from_row(row, i):
-    with open(cardoutputfolder + str(i) + '.tex', 'w') as ofile:
-        # print("starting card " + str(i))
-        # print(row)
-
-        card_text = "\\begin{tikzpicture}[scale=0.86, backbox/.style= {rectangle, minimum height = 2.2cm," \
-                   + " minimum width =2.2cm, rounded corners = 0.3cm, fill=white, opacity=0.75}]\n "
-        card_text = card_text + "\\node [rectangle, minimum width = 6.2cm, minimum height = 8.5cm, fill=black] at (4,5){};\n"
-        card_text = card_text + '\\node at (4,5){\\includegraphics[width=6cm, max height = 8.3cm, keepaspectratio]{' + images_folder + row["BackgroundImg"] + '}};\n'
-        # format the card
-        # name
-        card_text = card_text + "\\node [rectangle, minimum width=4cm, minimum height = 0.6cm,rounded corners = 0.1cm, fill=white, opacity=0.6] at (4, 9.2){\\large{" + row["Name"] + "}};\n"
-        # default symbols
-        card_text = card_text + '\\node at(1, 9.2){\\includegraphics[' + iconwidth + ']{' + icons_folder + initImg + '}};\n'
-        card_text = card_text +" \\node at (1, 9.2){\\Large{\\textbf{" + row['Initiative'] +"}}};\n"
-        card_text = card_text + '\\node at (1.1, 8.2){\\includegraphics[' + iconwidth + ']{' + icons_folder + mvImg + '}};\n'
-        card_text = card_text + " \\node at (1, 8.2){\\Large{\\textbf{" + row['Movement'] +"}}};\n"
-
-        # always high block
-        card_text = card_text + attack_box(0, 0, 1, 7.5)
-
-        if int(row["OneUse"]) > 0:
-             card_text = card_text + "\\node at (7,9.2)[circle, fill = red]{\\large{\\textbf{O}}};\n"
-
-
-        # textbox
-        card_text = card_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum height =1.5cm, rounded corners = 0.3cm, " \
-                    + "text width = 5.4cm]  at (4, 3.5){\\small{" + row['Text'] +"}};\n"
-        #set info
-        card_text = card_text + "\\node[rectangle, fill = white, opacity = 0.75, minimum width=6cm, minimum height =0.8cm, " \
-                + "rounded corners = 0.3cm, text width = 5.8cm]  at (4, 0.7){" \
-                +  row['Slot type'] + " : " + row['Slot name'] + "};\n"
 
         card_text = card_text + "\\end{tikzpicture}\n"
         # ofile.write(header_text)
@@ -147,7 +135,8 @@ def create_frame_sheet(frame, i):
         frame_text = frame_text + "\\node [rectangle, minimum width = 6.2cm, minimum height = 8.5cm, fill=black!70!white!30] at (4,5){};\n"
         frame_text = frame_text + '\\node at (4,5){\\includegraphics[width=6cm, max height = 8.3cm, keepaspectratio]{' + images_folder + frame["BackgroundImg"] + '}};\n'
         # name
-        frame_text = frame_text + "\\node [rectangle, minimum width=4.3cm, minimum height = 1cm,rounded corners = 0.1cm, fill=white, opacity=0.75] at (3.3, 9){\\large{" + frame["Name"] + "}};\n"
+        frame_text = frame_text + "\\node [rectangle, minimum width=4.3cm, minimum height = 1cm,rounded corners = 0.1cm, fill=white, opacity=0.75]" +\
+                            "at (3.3, 9){\\large{" + frame["Name"] + "}\n\\emph{" + frame["Faction"] + "}};\n"
         
         # movement
         frame_text = frame_text + '\\node at (7,9){\\includegraphics[' + iconwidth + ']{' + icons_folder + mvImg + '}};\n'
@@ -185,21 +174,21 @@ if __name__ == "__main__":
             for row in reader:
                 i = i + 1
                 if int(row["Changed"]) > 0:
-                        allfile.write(make_card_from_row(row, i))
+                        allfile.write(make_card_from_row(row, i, CardTypeEnum.WEAPON))
 
         with open(booster_actions_file, "r") as facsvfile:
             reader = csv.DictReader(facsvfile)
             for row in reader:
                 i = i + 1
                 if int(row["Changed"]) > 0:
-                        allfile.write(make_card_from_row(row, i))
+                        allfile.write(make_card_from_row(row, i, CardTypeEnum.BOOSTER))
 
         with open(pilot_actions_file, "r") as facsvfile:
             reader = csv.DictReader(facsvfile)
             for row in reader:
                 i = i + 1
                 if int(row["Changed"]) > 0:
-                        allfile.write(make_pilot_from_row(row, i))
+                        allfile.write(make_card_from_row(row, i, CardTypeEnum.PILOT))
 
         with open(general_action_file, "r") as gencsvfile:
             reader = csv.DictReader(gencsvfile)
@@ -208,7 +197,7 @@ if __name__ == "__main__":
                 if int(row["Changed"]) > 0:
                     for img in frameBackgrounds:
                         row["BackgroundImg"] = img
-                        allfile.write(make_card_from_row(row, i))
+                        allfile.write(make_card_from_row(row, i, CardTypeEnum.BASIC))
         j = 0
         with open(frames_file, "r") as fcsvfile:
             reader = csv.DictReader(fcsvfile)
