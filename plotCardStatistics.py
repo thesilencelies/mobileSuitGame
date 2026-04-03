89e4ab00d37d7b81376ab22b7b51aa099bb2b963
Stephen Lilico <lilico@acsl.com>
2026-04-03 19:45:23 +0900

    adding more cards

---
+++ plotCardStatistics.py
@@ -0,0 +1,17 @@
+import matplotlib as plt
+import numpy as np
+
+
+weapon_actions_file = 'Weapon actions.csv'
+general_action_file = 'Basic actions.csv'
+pilot_actions_file = 'Pilot actions.csv'
+booster_actions_file = 'Booster actions.csv'
+frames_file = 'Frames.csv'
+
+# What kind of metadata are we interested in? 
+
+# Initiative distribution
+# Movement distribution
+
+
+# Maybe we want a UI which allows comparing any 2 stats against each other - seems like a good use case for AI
